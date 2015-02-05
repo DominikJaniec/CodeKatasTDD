4d3e7188ad2b74ba7708bc9b5691645e21bc2a45
Dominik Janiec <dominikjaniec@gmail.com>
2015-02-05 16:56:18 +0100

    Kata.01 - String Calculator: Refactoring StringCalculator class

--- Kata.01 - String Calculator/KataStringCalculator/StringCalculator.cs
+++ Kata.01 - String Calculator/KataStringCalculator/StringCalculator.cs
@@ -1,36 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace KataStringCalculator
 {
     public class StringCalculator
     {
+        private readonly char[] Splitters = new[] { ',' };
+
         public int Add(string numbers)
         {
-            if (String.Empty == numbers)
-            {
-                return 0;
-            }
-            else
-            {
-                int sum = 0;
+            int sum = 0;
 
-                string[] numbersArray = numbers.Split(',');
-                foreach (string number in numbersArray)
-                {
-                    sum += ParseNumber(number);
-                }
-
-                return sum;
+            foreach (string number in SplitNumbers(numbers))
+            {
+                sum += ParseNumber(number);
             }
+
+            return sum;
         }
 
         private int ParseNumber(string number)
         {
             return Int32.Parse(number);
+        }
+
+        private string[] SplitNumbers(string numbers)
+        {
+            return numbers.Split(Splitters, StringSplitOptions.RemoveEmptyEntries);
         }
     }
 }
