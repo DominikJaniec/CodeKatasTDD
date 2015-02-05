35190516a0a95e269c8c2947cf2470ccc2387e4c
Dominik Janiec <dominikjaniec@gmail.com>
2015-02-05 16:49:25 +0100

    Kata.01 - String Calculator: Testing '1,2' - Red

--- Kata.01 - String Calculator/StringCalculatorTests/StringCalculatorAddTests.cs
+++ Kata.01 - String Calculator/StringCalculatorTests/StringCalculatorAddTests.cs
@@ -38,5 +38,13 @@ namespace StringCalculatorTests
 
             Assert.AreEqual(2, result);
         }
+
+        [TestMethod]
+        public void ForOneTwo_ReturnThree()
+        {
+            int result = Calculator.Add("1,2");
+
+            Assert.AreEqual(3, result);
+        }
     }
 }
