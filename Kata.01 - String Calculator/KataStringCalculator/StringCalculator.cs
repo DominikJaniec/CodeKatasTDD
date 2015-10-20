b6f7b5f0639d2d523e5e878c3fd58d2b7c3302c0
Dominik Janiec <dominikjaniec@gmail.com>
2015-10-20 16:30:46 +0200

    Kata.01 - String Calculator: Little refactoring

--- Kata.01 - String Calculator/KataStringCalculator/StringCalculator.cs
+++ Kata.01 - String Calculator/KataStringCalculator/StringCalculator.cs
@@ -8,17 +8,20 @@ namespace KataStringCalculator
     {
         public int Add(string numbers)
         {
-            if (string.IsNullOrEmpty(numbers))
-                return 0;
-
             return GetNumbers(numbers).Sum();
         }
 
         private static IEnumerable<int> GetNumbers(string numbers)
         {
-            return numbers
-                .Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+            return GetNumbersAsStrings(numbers)
                 .Select(Int32.Parse);
+        }
+
+        private static readonly char[] Splitters = new[] { ',' };
+
+        private static IEnumerable<string> GetNumbersAsStrings(string numbers)
+        {
+            return numbers.Split(Splitters, StringSplitOptions.RemoveEmptyEntries);
         }
     }
 }
