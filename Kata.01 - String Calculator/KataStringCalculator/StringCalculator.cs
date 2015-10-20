d70916cac32d98cd06b414a3474c888382a6024d
Dominik Janiec <dominikjaniec@gmail.com>
2015-10-20 17:30:03 +0200

    Kata.01 - String Calculator: Test for defining custom delimeter - Green

--- Kata.01 - String Calculator/KataStringCalculator/StringCalculator.cs
+++ Kata.01 - String Calculator/KataStringCalculator/StringCalculator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace KataStringCalculator
 {
@@ -17,11 +18,48 @@ namespace KataStringCalculator
                 .Select(Int32.Parse);
         }
 
-        private static readonly char[] Splitters = new[] { ',', '\n' };
+        private static readonly string[] DefaultSplitters = { ",", "\n" };
+
+        private static readonly Regex CustomDelimiterDefinition
+            = new Regex(@"^//(?<CUSTOM_DELIMITER>.+?)\n(?<NUMBERS>\d.+)$", RegexOptions.Compiled);
 
         private static IEnumerable<string> GetNumbersAsStrings(string numbers)
         {
-            return numbers.Split(Splitters, StringSplitOptions.RemoveEmptyEntries);
+            var numbersSequence = numbers;
+            var delimiterStringArray = DefaultSplitters;
+
+            FindCustomDelimiters(
+                numbers,
+                ref delimiterStringArray,
+                ref numbersSequence);
+
+            return numbersSequence.Split(
+                delimiterStringArray,
+                StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        private static void FindCustomDelimiters(
+            string baseNumberSequence,
+            ref string[] delimiter,
+            ref string numbers)
+        {
+            var customDefinitions = CustomDelimiterDefinition
+                .Match(baseNumberSequence);
+
+            if (customDefinitions.Success)
+            {
+                var customDelimiter = customDefinitions
+                    .Groups["CUSTOM_DELIMITER"]
+                    .Value;
+
+                delimiter = new[] { customDelimiter }
+                    .Concat(DefaultSplitters)
+                    .ToArray();
+
+                numbers = customDefinitions
+                    .Groups["NUMBERS"]
+                    .Value;
+            }
         }
     }
 }
