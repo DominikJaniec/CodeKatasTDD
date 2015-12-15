0c1b4c95535ff532c66fa5e2de614c0f7af5818a
Dominik Janiec <dominikjaniec@gmail.com>
2015-12-15 13:08:00 +0100

    Kata.02 - Roman Numerals: Representation of 2 - Green

--- Kata.02 - Roman Numerals/KataRomanNumerals/Romaner.cs
+++ Kata.02 - Roman Numerals/KataRomanNumerals/Romaner.cs
@@ -1,10 +1,19 @@
-﻿namespace KataRomanNumerals
+﻿using System;
+
+namespace KataRomanNumerals
 {
     public static class Romaner
     {
         public static string ToRoman(int value)
         {
-            return "I";
+            return Repeat(RomanUnity, times: value);
+        }
+
+        private const char RomanUnity = 'I';
+
+        private static string Repeat(char character, int times)
+        {
+            return new String(character, times);
         }
     }
 }
