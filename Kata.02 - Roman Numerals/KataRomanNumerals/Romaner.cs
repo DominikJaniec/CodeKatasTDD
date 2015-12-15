39f3b9018b2209a3bb8c0967f3070792a3e82ea4
Dominik Janiec <dominikjaniec@gmail.com>
2015-12-15 13:03:47 +0100

    Kata.02 - Roman Numerals: Representation of 1 - Red

---
+++ Kata.02 - Roman Numerals/KataRomanNumerals/Romaner.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KataRomanNumerals
+{
+    public static class Romaner
+    {
+        public static string ToRoman(int value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Kata.02 - Roman Numerals/RomanNumeralsTests/RomanerTests.cs
@@ -0,0 +1,15 @@
+﻿using KataRomanNumerals;
+using Xunit;
+
+namespace RomanNumeralsTests
+{
+    public class RomanerTests
+    {
+        [Fact]
+        public void One_ShouldBe_I()
+        {
+            var romanNumber = Romaner.ToRoman(1);
+            Assert.Equal("I", romanNumber);
+        }
+    }
+}
