b805f9df7263baf28d27e4337f40464a82946e34
Dominik Janiec <dominikjaniec@gmail.com>
2015-12-15 14:07:25 +0100

    Kata.02 - Roman Numerals: Sample by brute force - Green

--- Kata.02 - Roman Numerals/RomanNumeralsTests/RomanerTests.cs
+++ Kata.02 - Roman Numerals/RomanNumeralsTests/RomanerTests.cs
@@ -39,5 +39,27 @@ namespace RomanNumeralsTests
             var romanNumber = Romaner.ToRoman(5);
             Assert.Equal("V", romanNumber);
         }
+
+        [Theory]
+        [InlineData(4, "IV")]
+        [InlineData(7, "VII")]
+        [InlineData(19, "XIX")]
+        [InlineData(40, "XL")]
+        [InlineData(95, "XCV")]
+        [InlineData(900, "CM")]
+        [InlineData(1025, "MXXV")]
+        [InlineData(1995, "MCMXCV")]
+        [InlineData(2000, "MM")]
+        [InlineData(1956, "MCMLVI")]
+        [InlineData(2011, "MMXI")]
+        [InlineData(1954, "MCMLIV")]
+        [InlineData(1990, "MCMXC")]
+        [InlineData(2014, "MMXIV")]
+        [InlineData(3338, "MMMCCCXXXVIII")]
+        public void Sample_ShouldBe_Correct(int sampleValue, string expectedRomanNumber)
+        {
+            var romanNumber = Romaner.ToRoman(sampleValue);
+            Assert.Equal(expectedRomanNumber, romanNumber);
+        }
     }
 }
